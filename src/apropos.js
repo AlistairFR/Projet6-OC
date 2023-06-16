4357534fed7452eb7250d2a1e98e6fe150bb61d5
AlistairFR <jbwinckel62370@gmail.com>
2023-06-16 20:46:15 +0200

    AJOUT page a propos

---
+++ src/apropos.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/apropos.scss';
+import NavBar from './components/Navbar';
+import Footer from './components/Footer';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <NavBar />
+    <Footer />
+  </React.StrictMode>
+);
