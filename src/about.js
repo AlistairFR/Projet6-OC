f041e8214e5540c5087fb2b22e297a5a29ef8aab
AlistairFR <jbwinckel62370@gmail.com>
2023-06-16 22:03:15 +0200

    AJOUT gallery.scss + Rename about.js et scss

--- src/about.js
+++ src/about.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/apropos.scss';
+import './styles/about.scss';
 import NavBar from './components/Navbar';
 import Footer from './components/Footer';
 
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,9 +1,11 @@
-import LocationList  from 'https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P9+React+1/logements.json';
-import LocationCard from './LocationCard'
+import LocationList  from '../data/logements.json';
+import LocationCard from './LocationCard';
+
+import '../styles/Gallery.scss';
 
 function Gallery() {
     return(
-        <main className='Gallery'>
+        <main className='gallery'>
             {LocationList.map((e) => (
                 <LocationCard key={e.id} id={e.id} cover={e.cover} title={e.title} />
             ))}
