3204ddefe8168c361a9642829ded31ca0fcfb5fe
AlistairFR <jbwinckel62370@gmail.com>
2023-06-20 12:06:59 +0200

    AJOUT Link locationCards vers props.id

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,9 +1,11 @@
+import {Link} from 'react-router-dom';
+
 function Card(props) {
     return(
-        <div className="card">
+        <Link to={`/${props.id}`} className="card">
             <div className="card-img-container"><img className="card-img" src={props.cover} alt={`Cover de ${props.title}`} /></div>
             <span className="card-title"> {props.title} </span>
-        </div>
+        </Link>
     )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
-import '../styles/Navbar.scss'
-import kasalogo from '../images/kasa-logo.png'
+import '../styles/Navbar.scss';
+import kasalogo from '../images/kasa-logo.png';
 import {Link} from 'react-router-dom';
 
 function NavBar() {
