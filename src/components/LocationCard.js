b3b33aa43b7ada1ad25c9cf790968c8e1715656f
AlistairFR <jbwinckel62370@gmail.com>
2023-06-20 12:12:59 +0200

    FIX url Link locationCards

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -2,7 +2,7 @@ import {Link} from 'react-router-dom';
 
 function Card(props) {
     return(
-        <Link to={`/${props.id}`} className="card">
+        <Link to={`/housing/${props.id}`} className="card">
             <div className="card-img-container"><img className="card-img" src={props.cover} alt={`Cover de ${props.title}`} /></div>
             <span className="card-title"> {props.title} </span>
         </Link>
