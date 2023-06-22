3d10d4c5134bcf7305bed8d1a835b69a9b26130c
AlistairFR <jbwinckel62370@gmail.com>
2023-06-22 17:52:15 +0200

    FIX CSS navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,31 @@
 import '../styles/Navbar.scss';
 import kasalogo from '../images/kasa-logo.png';
-import {Link} from 'react-router-dom';
+import {Link, useLocation} from 'react-router-dom';
 
 function NavBar() {
+    const location = useLocation();
     return (
         <nav>
             <img src={kasalogo} alt="Logo de Kasa" />
             <div>
-                <Link to="/">Accueil</Link>
-                <Link to="/about">A Propos</Link>
+                <Link className={
+                    location.pathname === '/'
+                        ? 'active NavBar-Links'
+                        : 'NavBar-Links'
+                    }
+                    to="/"
+                >
+                    Accueil
+                </Link>
+                <Link className={
+                    location.pathname === '/about'
+                        ? 'active NavBar-Links'
+                        : 'NavBar-Links'
+                    }
+                    to="/about"
+                >
+                    A Propos
+                </Link>
             </div>
         </nav>
     )
