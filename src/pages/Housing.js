ccd39ece77033e63b6e8e4593e9380e2efe7bf93
AlistairFR <jbwinckel62370@gmail.com>
2023-06-26 21:01:55 +0200

    AJOUT collapse pour logements

--- src/pages/Housing.js
+++ src/pages/Housing.js
@@ -3,6 +3,7 @@ import LocationList from "../data/logements.json"
 import Error from "../pages/Error"
 import Showcase from "../components/Showcase"
 import Infos from "../components/Infos"
+import Collapse from "../components/Collapse"
 
 function Housing() {
     const { id } = useParams()
@@ -22,6 +23,24 @@ function Housing() {
                 owner={location.host.name}
                 ownerImg={location.host.picture}
             />
+            <div className="housing-collapse-container">
+                <Collapse
+                    label="Description"
+                    class="housing-collapse-container-description"
+                    text={<p>{location.description}</p>}
+                />
+                <Collapse
+                    label="Équipements"
+                    class="location-collapse-container-equipments"
+                    text={
+                        <ul>
+                            {location.equipments.map((equipments, index) => (
+                                <li key={index}>{equipments}</li>
+                            ))}
+                        </ul>
+                    }
+                />
+            </div>
         </div>
     )
 }
