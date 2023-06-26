3268453bb85127fc01c91bc380bdb6d80cd78bde
AlistairFR <jbwinckel62370@gmail.com>
2023-06-26 20:56:57 +0200

    AJOUT appel AboutCollapses dans about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from "../components/Banner";
+import AboutCollapses from '../components/AboutCollapses';
 import bannerimg from "../images/aboutbanner.jpg"
 import '../styles/About.scss';
 
@@ -7,6 +8,7 @@ function About() {
   return (
     <div className='about'>
       <Banner bannerimg = {bannerimg} />
+      <AboutCollapses />
     </div>
   )
 }
