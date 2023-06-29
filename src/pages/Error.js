8282d019a1c4dcadee079acf4341ace366a7f473
AlistairFR <jbwinckel62370@gmail.com>
2023-06-29 11:05:26 +0200

    AJOUT SCSS Error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
+import '../styles/Error.scss'
 
 function Error() {
     return(
-        <div>
-            <h1>404</h1>
-            <p>Oups! La page que vous demandez n'existe pas.</p>
-            <Link to={'/'}>Retourner sur la page d'accueil</Link>
+        <div className="error-main">
+            <h1 className="error-title">404</h1>
+            <p className="error-text">Oups! La page que vous demandez n'existe pas.</p>
+            <Link className="error-link" to={'/'}>Retourner sur la page d'accueil</Link>
         </div>
     )
 }
